b1124ec46b224dedbd83db8edffee72344c6a5ce
biuaxia <biuaxia@qq.com>
2021-03-03 16:29:47 +0800

    feat: 单独测试公文及关联数据获取

--- src/test/java/cn/biuaxia/odm/OaDataMigrationApplicationTests.java
+++ src/test/java/cn/biuaxia/odm/OaDataMigrationApplicationTests.java
@@ -1,10 +1,12 @@
 package cn.biuaxia.odm;
 
 import cn.biuaxia.odm.generate.domain.EdocSummary;
+import cn.biuaxia.odm.generate.service.EdocSummaryService;
 import cn.hutool.core.collection.CollUtil;
 import cn.hutool.core.convert.Convert;
 import cn.hutool.core.date.TimeInterval;
 import cn.hutool.core.io.FileUtil;
+import cn.hutool.core.util.RandomUtil;
 import cn.hutool.core.util.ReflectUtil;
 import cn.hutool.core.util.StrUtil;
 import cn.hutool.extra.spring.SpringUtil;
@@ -13,6 +15,7 @@ import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.util.comparator.Comparators;
@@ -25,10 +28,33 @@ import java.util.List;
 @SpringBootTest(classes = OaDataMigrationApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class OaDataMigrationApplicationTests {
 
+    /**
+     * 计时器
+     */
+    private final TimeInterval interval = new TimeInterval();
+
+    @Autowired
+    private EdocSummaryService edocSummaryService;
+
     @Test
     public void contextLoads() {
-        TimeInterval interval = new TimeInterval();
         interval.start();
+        // getAllDataExistMapperBean(interval);
+        List<EdocSummary> edocSummaryList =
+                edocSummaryService.list(
+                        Wrappers.<EdocSummary>lambdaQuery()
+                                .eq(EdocSummary::getState, 3)
+                );
+        log.info("正常结束的公文主表数据获取完成, 共计 [{}] 条, 耗时: [{}s], 开始分析数据...", edocSummaryList.size(), interval.intervalSecond());
+        log.info("随机挑选数据得到: [{}]", JSONUtil.toJsonPrettyStr(
+                edocSummaryList.get(RandomUtil.randomInt(0, edocSummaryList.size() - 1))
+        ));
+    }
+
+    /**
+     * 获取所有有数据的表对应的Mapper对象
+     */
+    private void getAllDataExistMapperBean() {
         String projectPath = System.getProperty("user.dir");
         log.info("currentProjectPath: [{}]", projectPath);
         String mapperBeanPath = StrUtil.format("{}/src/main/java/cn/biuaxia/odm/generate/mapper", projectPath);
@@ -58,12 +84,8 @@ public class OaDataMigrationApplicationTests {
                 hasDataTableMap.put(item, count);
             }
         });
-        log.info("耗时: {}s", interval.intervalSecond());
-        log.debug("hasDataTableList: [{}]",
-                JSONUtil.toJsonPrettyStr(hasDataTableList));
-        log.info("size: [{}], hasDataTableMap: [{}]",
-                hasDataTableMap.size(),
-                JSONUtil.toJsonPrettyStr(hasDataTableMap));
+
+        log.info("获取所有有数据的mapperBean完成, 共计 [{}] 条, 耗时: [{}s]", hasDataTableList.size(), interval.intervalSecond());
     }
 
 }
